3078480b3d284a00081a74bead87f4dcf5841edb
anonymous-pc <kazkoreeda@gmail.com>
2017-11-14 14:39:36 +0900

    2017.11.14 commit

---
+++ app/src/main/java/com/adsoft/helloworld/HelloView.java
@@ -0,0 +1,27 @@
+package com.adsoft.helloworld;
+
+/**
+ * Created by anonymous-pc on 2017/09/12.
+ */
+       import android.content.Context;
+        import android.graphics.Canvas;
+        import android.graphics.Color;
+        import android.graphics.Paint;
+        import android.view.View;
+
+//HelloWorld
+public class HelloView extends View {//(7)
+    //コンストラクタ
+    public HelloView(Context context) {
+        super(context);
+        setBackgroundColor(Color.WHITE);//(8)
+    }
+
+    //描画
+    @Override
+    protected void onDraw(Canvas canvas) {//(9)
+        Paint paint = new Paint();
+        paint.setTextSize(48);
+        canvas.drawText("Hello World!", 0, 48, paint);//(10)
+    }
+}
---
+++ app/src/main/java/com/adsoft/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.adsoft.helloworld;
+
+/**
+ * Created by anonymous-pc on 2017/09/12.
+ */
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.view.Window;
+
+//HelloWorld
+public class HelloWorld extends Activity {//(2)(3)
+    //アクティビティ起動時に呼ばれる
+    @Override
+    public void onCreate(Bundle bundle) {//(4)
+        super.onCreate(bundle);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//(5)
+        setContentView(new HelloView(this));//(6)
+    }
+}
